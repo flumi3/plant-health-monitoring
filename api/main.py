7e8997e580263f1ef6a90161616c89266279afaf
Axel Stark <axelstark@hotmail.de>
2022-06-15 11:42:55 +0200

    Added OTA-Update endpoint in backend

--- api/main.py
+++ api/main.py
@@ -1,7 +1,18 @@
+import json
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return "Running on port 8000"
+
+
+@app.get("/firmwareVersion")
+async def check_version_number():
+    with open("firmware/version.json") as file:
+        data = json.load(file)
+        return data['current_version']
+
+app.mount("/firmware",StaticFiles(directory="firmware"), name="firmware")
