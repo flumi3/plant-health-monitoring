7310b55ef1dfcfaba33a20e0ab647eb7526e272a
Sebastian Flum <sebastian.flum.dev@gmail.com>
2022-07-06 15:55:47 +0200

    added functionality for monitoring several devices

--- api/models.py
+++ api/models.py
@@ -1,11 +1,19 @@
-from datetime import datetime
-from sqlalchemy import Boolean, Column, Integer, Float
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, Integer, Float, String
+from sqlalchemy.orm import relationship, backref
 
 from database import Base
 
+
+class Device(Base):
+    __tablename__ = "device"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False, unique=True)
+    device_hash = Column(String, nullable=False, unique=True)
+
+
 class PlantData(Base):
-    __tablename__ = "Plant Data"
+    __tablename__ = "plant_data"
 
     id = Column(Integer, primary_key=True, index=True)
     air_temperature = Column(Float, nullable=False)
@@ -13,3 +21,7 @@ class PlantData(Base):
     soil_temperature = Column(Float, nullable=False)
     soil_humitidy = Column(Float, nullable=False)
     timestamp = Column(Integer, nullable=False)
+    
+    # 1 to 1 relationship with device
+    device_id = Column(Integer, ForeignKey("device.id"))
+    device = relationship("Device", backref=backref("plant_data", uselist=False))
