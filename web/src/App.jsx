936e2c4d4f830e62dc109155d2658b47075e2175
Sebastian Flum <sebastian.flum.dev@gmail.com>
2022-07-09 14:37:07 +0200

    added devices page

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css';
+import "./App.css";
 import { createTheme, ThemeProvider } from "@mui/material";
-import TopBar from './components/TopBar';
-import MeasurementCard from './components/MeasurementCard';
+import TopBar from "./components/TopBar";
+import Devices from "./pages/Devices";
 
 const App = () => {
 
@@ -30,41 +30,9 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <TopBar />
-      <div className="container">
-        <MeasurementCard className="air-temp" title="Air Temperature" value="23°" />
-        <MeasurementCard className="air-humidity" title="Air Humidity" value="0" />
-        <MeasurementCard className="soil-temp" title="Soil Temperature" value="15°" />
-        <MeasurementCard className="soil-humidity" title="Soil Humidity" value="0" />
-      </div>
+      <Devices />
     </ThemeProvider>
   );
 }
-
-// const axios = require('axios').default;
-
-// const API_URL = 'http://localhost:8000'
-
-// const axios_instance = axios.create({
-//   baseURL: API_URL,
-//   headers: { 'Access-Control-Allow-Origin': '*' }
-// });
-
-// async function fetchData() {
-//   var response = await axios_instance.get('/plant-data');
-//   console.log(response.data)
-// }
-// fetchData();
-
-// async function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           hello World
-//         </p>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
---
+++ web/src/pages/Devices.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { Typography } from "@mui/material";
+import "./Devices.css"
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+export default function Devices() {
+    const [ devices, setDevices ] = useState([]);
+
+    useEffect(() => {
+        // get devices from backend
+        const getDevices = async () => {
+            const base = process.env.REACT_APP_API_SERVER_URL;
+            const url = base + "/devices";
+            fetch(url)
+                .then(response => response.json())
+                .then(data => setDevices(data));
+        }
+        getDevices();
+    }, []);
+
+    return (
+        <div>
+            <Typography className="devices-header" variant="h4" fontWeight="bold">
+                Devices
+            </Typography>
+            <div className="devices-content">
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>
+                                    <Typography fontWeight="bold">
+                                        Device Name
+                                    </Typography>
+                                </TableCell>
+                                <TableCell align="right">
+                                    <Typography fontWeight="bold">
+                                        Device ID
+                                    </Typography>
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {devices.map((device) => (
+                                <TableRow
+                                    key={device.name}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    hover
+                                    onClick={() => {
+                                        console.log("click");
+                                    }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {device.name}
+                                    </TableCell>
+                                    <TableCell align="right">{device.device_hash}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        </div>
+    );
+};
