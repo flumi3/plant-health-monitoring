7c31e2d163b38e88cd652515f160e5e6d61daf70
Sebastian Flum <sebastian.flum.dev@gmail.com>
2022-08-29 18:11:31 -0500

    formatting and docs

--- emulator/emulator.py
+++ emulator/emulator.py
@@ -1,23 +1,32 @@
-from paho.mqtt.client import Client, MQTTMessage
-from random import randint
 import time
 import json
+from random import randint
+
+from paho.mqtt.client import Client, MQTTMessage
 
 CLIENT_ID = "EMULATOR_A"
-def create_random_data():
-    return {
-        "airtemp": randint(-10,30),
-        "airhumidity": randint(0,100),
-        "soiltemp": randint(-10,30),
-        "soilmoisture": randint(0,30)
-    }
 MESSAGE = None
-def on_message(client: Client, userdata, message: MQTTMessage): 
+
+
+def create_random_data():
+    """Create random device data in order to mock a device."""
+    return {
+        "airtemp": randint(-10, 30),
+        "airhumidity": randint(0, 100),
+        "soiltemp": randint(-10, 30),
+        "soilmoisture": randint(0, 30),
+    }
+
+
+def on_message(client: Client, userdata, message: MQTTMessage):
+    """Callback for when a message is received from the broker."""
     global MESSAGE
     MESSAGE = message
 
+
 def on_connect(client: Client, userdata, flags: dict, rc: int):
-    client.subscribe(f'/{CLIENT_ID}/reset')
+    """Callback for when the client connects to the broker."""
+    client.subscribe(f"/{CLIENT_ID}/reset")
     print(rc)
 
 
@@ -26,9 +35,9 @@ client.on_message = on_message
 client.on_connect = on_connect
 client.connect("193.197.229.59")
 
-while True: 
+while True:
     data = create_random_data()
     print("Publish....")
-    client.publish(f'/{CLIENT_ID}/plant_data',payload = json.dumps(data))
+    client.publish(f"/{CLIENT_ID}/plant_data", payload=json.dumps(data))
     client.loop()
     time.sleep(5)
